87be2120391d1f2db98e852259c7920b1dbe2d64
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-07-19 00:39:21 +0100

    Fix: updated books-all

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -48,7 +48,7 @@ except Author.DoesNotExist:
 library_name = "Central Library"
 try:
     library = Library.objects.get(name=library_name)
-    books_in_library = Book.objects.filter(library=library)
+    books_in_library = library.books.all()  
     print(f"\n🏛️ Books in {library_name}:")
     for book in books_in_library:
         print(f"- {book.title}")
