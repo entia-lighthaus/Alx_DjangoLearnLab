9a75d704fbe176c73084617375ffcea090e6b60f
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-07-19 16:15:27 +0100

    Add role-based access control for admin, librarian, member views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+    path('admin-only/', views.admin_view, name='admin_view'),
+    path('librarian-only/', views.librarian_view, name='librarian_view'),
+    path('member-only/', views.member_view, name='member_view'),
 ]
