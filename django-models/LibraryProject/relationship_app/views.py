88b0a26923dedceda21ccac49121d81ff54881fb
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-07-19 15:13:02 +0100

    Add user authentication using built-in forms and login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,24 @@
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .models import Book  # Needed for list_books view
 from .models import Library  # Required for the check (even if Library is already used elsewhere)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # logs the user in after successful registration
+            return redirect('library_list')  # or wherever you'd like to redirect
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
 
 
 # Function-based view to list all books
