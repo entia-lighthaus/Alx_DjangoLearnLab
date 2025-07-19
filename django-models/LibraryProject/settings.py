d26bded2743cf311d47659ba27a35738dbe9daa4
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-07-19 16:01:50 +0100

    first commit

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
-    'LibraryProject.relationship_app',
+    'relationship_app',
 ]
 
 MIDDLEWARE = [
