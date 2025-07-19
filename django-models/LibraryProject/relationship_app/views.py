fba7b76e6993f15ca497e5d89d09bb976e0ec449
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-07-19 01:56:55 +0100

    Fix list_books view to pass checks: use Book.objects.all() and correct template path

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse
+from .models import Book, Library  # Assuming these models exist
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all() 
+    return render(request, 'list_books.html', {'books': books})
+
+
+from django.views.generic.detail import DetailView
+
+# Class-based view to display library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
