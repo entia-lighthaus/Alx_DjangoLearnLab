6092466a4e3c3a9dee7a61eee41cc25a86d2d7e2
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-07-19 14:55:57 +0100

    Add user authentication using built-in forms and login

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,25 @@
 # relationship_app/urls.py
 # Create this file in your relationship_app directory
+
+
+from django.contrib.auth import views as auth_views
+from . import views
+
 from django.urls import path
 from .views import list_books, LibraryDetailView
 # from .views import LibraryListView  # Commented out for now
 
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+]
+
+urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
