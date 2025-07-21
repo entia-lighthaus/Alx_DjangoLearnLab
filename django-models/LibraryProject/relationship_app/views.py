4a7ef93ca1cb627a6f0a558122bdbde7b20353e9
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-07-21 11:23:45 +0100

    fixing issues for alx checks

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -35,6 +35,11 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  # Get all books in this library
+        return context
+
 
 def is_admin(user):
     """
