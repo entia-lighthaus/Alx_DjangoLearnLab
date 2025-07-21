1f81080b0ce604c864ed4b6cfb24056d3c60764f
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-07-21 12:30:37 +0100

    updated aythentication urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,7 +7,13 @@ from django.urls import path
 from django.urls import path
 from .import views
 
+
 urlpatterns = [
+     # Authentication URLs
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+
     # Function-based view for listing all books
     path('books/', views.list_books, name='list_books'),
     
@@ -18,11 +24,6 @@ urlpatterns = [
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/', views.edit_book, name='edit_book'),
     path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
-    
-    # Authentication URLs
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', views.register, name='register'),
     
     # Home and main pages
     path('', views.home_view, name='home'),
