de2efae47d87b8da1886c044efcc8deaeac374a0
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-08-12 13:49:55 +0100

    updated urls.py to pass ALX checks

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,11 +20,10 @@ urlpatterns = [
 
     # Blog Post CRUD URLs
     path('posts/', views.PostListView.as_view(), name='post-list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
-
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
 
 
