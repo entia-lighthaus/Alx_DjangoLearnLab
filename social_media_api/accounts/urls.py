3e6062144234d14627b54912f356276390b59a87
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-08-21 08:57:13 +0100

    debugged accounts/urls.py to handle following and unfollowing users

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,10 +5,14 @@ from .views import UserViewSet, RegisterView, LoginView, ProfileView
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
+
+
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', UserViewSet.as_view({'post': 'follow'}), name='follow-user'), # This line was added to handle following users
+    path('unfollow/<int:user_id>/', UserViewSet.as_view({'post': 'unfollow'}), name='unfollow-user'), # This line was added to handle unfollowing users
 ]
 
 urlpatterns += router.urls
