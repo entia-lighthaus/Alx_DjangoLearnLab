d34eb0eebc13f03f6d56ef4b377949cc5484ac0d
Innocentia Neriah Duru <entiaduru@gmail.com>
2025-08-22 12:44:34 +0100

    Update settings.py for Render deployment

--- social_media_api/notifications/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import NotificationListView
-
-
-# This URL configuration handles the notifications endpoint, allowing users to view their notifications.
-urlpatterns = [
-    path('', NotificationListView.as_view(), name='notifications'),
-]
